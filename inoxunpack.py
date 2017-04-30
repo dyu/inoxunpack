9fa9480292a4f1e974dce90155cf80773afdceb2
dyu <david.yu.ftw@gmail.com>
2017-04-30 17:51:56 +0800

    minor fixes

--- inoxunpack.py
+++ inoxunpack.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import argparse
 import json
@@ -35,7 +35,7 @@ Install Guide:
 
   1. Visit chrome://extensions in your browser.
   2. Ensure that the Developer mode checkbox in the top right-hand corner is checked.
-  3. Click Load unpacked extension… to pop up a file-selection dialog.
+  3. Click Load unpacked extension to pop up a file-selection dialog.
   4. Navigate to the directory in which your extension files live, and select it.
 
   Alternatively, you can drag and drop the directory where your extension files
@@ -112,7 +112,7 @@ Install Guide:
 
   1. Visit chrome://extensions in your browser.
   2. Ensure that the Developer mode checkbox in the top right-hand corner is checked.
-  3. Click Load unpacked extension… to pop up a file-selection dialog.
+  3. Click Load unpacked extension to pop up a file-selection dialog.
   4. Navigate to {}
 
   Alternatively, you can drag and drop the directory where your extension files
